522876224afc56bad26041a0a6a3220530cae143
Matheus Miranda <matheusmiranda2809@gmail.com>
2020-08-25 22:19:02 -0300

    exe3 da lista 2 - requerimeto 2 - OK

--- Lista2/exe3.js
+++ Lista2/exe3.js
@@ -30,14 +30,26 @@ function requisito1(vet) { // vet aponta para vetor
     objeto.sexo = prompt(`Informe M ou F`).toUpperCase()
     objeto.altura = Number(prompt(`Informe altura`))
     objeto.idade = Number(prompt(`Informe idade`))
-    objeto.olhos = prompt(`Informe cor dos olhos - A - azul V - verde C - castanho`)
+    objeto.olhos = prompt(`Informe cor dos olhos - A - azul V - verde C - castanho`).toUpperCase()
     // insere no vetor
     vet.push(objeto) // insere no vetor (escopo global)
     console.log(`Habitante cadastrado com sucesso`)
 }
-function requisito2(vet) { // vet aponta para vetor
 
+function requisito2(vet) { // vet aponta para vetor
+    var soma = 0
+    var qtde = 0
+    for (var i = 0; i < vet.length; i++) {
+        if ((vet[i].olhos == 'C') && (vet[i].altura > 1.60)) {
+            soma = soma + vet[i].idade
+            qtde++
+        }
+    }
+    if (qtde != 0)
+        console.log(`A média é ${soma / qtde}`)
+    else console.log(`Não existe habitante para este cálculo`)
 }
+
 function requisito3(vet) { // vet aponta para vetor
 
 }
